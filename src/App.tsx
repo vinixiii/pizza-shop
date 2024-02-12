231b8b3cc698f54492eb0a1f3e2f29c5fe35d3ac
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2024-02-12 15:55:32 -0300

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from './components/ui/button';
+
 import './styles/global.css';
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Enviar</Button>
     </div>
   );
