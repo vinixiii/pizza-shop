010f17de9f06e767ac5102926bfc858f5efe33da
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2024-02-12 15:22:27 -0300

    chore: add shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+import './styles/global.css';
+
 export function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   );
 }
