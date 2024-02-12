dbf8211004984fe176ae336f89ccc9462f6aac96
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2024-02-12 18:06:32 -0300

    feat: add taost to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
 import { router } from './routes/router';
 
@@ -9,6 +10,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
 
       <RouterProvider router={router} />
     </HelmetProvider>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -21,7 +22,12 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInSchema) {
     console.log({ data });
+
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    toast.success('Enviamos um link de autenticação para o seu e-mail!', {
+      duration: 1500,
+    });
   }
 
   return (
